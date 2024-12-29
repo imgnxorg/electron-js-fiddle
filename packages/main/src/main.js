d0b226cf77220057823a27fab68d5acafaba5edb
imgnx <donaldwaynemoorejr@gmail.com>
2024-12-28 22:37:13 -0600

    Renderer

--- packages/main/src/main.js
+++ packages/main/src/main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+// const path = require("path");
+const chalk = () => import("chalk");
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
@@ -7,28 +8,62 @@ function createWindow() {
     height: 800,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
-    }
+      contextIsolation: true,
+      nodeIntegration: false,
+      webSecurity: true,
+      sandbox: true,
+    },
   });
 
+  mainWindow.webContents.session.webRequest.onHeadersReceived(
+    (details, callback) => {
+      callback({
+        responseHeaders: {
+          ...details.responseHeaders,
+          "Content-Security-Policy": [
+            "default-src 'self'",
+            "script-src 'self'",
+            "style-src 'self' 'unsafe-inline'",
+            "img-src 'self' data:",
+            "font-src 'self'",
+            "connect-src 'self'",
+          ].join("; "),
+        },
+      });
+    },
+  );
+
   // In development, load from dev server
-  if (process.env.NODE_ENV === 'development') {
-    mainWindow.loadURL('http://localhost:3000');
+  console.log(chalk.yellow(process.env.NODE_ENV));
+  if (process.env.NODE_ENV === "development") {
+    mainWindow.loadURL("http://localhost:3000");
     // Open the DevTools automatically
     mainWindow.webContents.openDevTools();
   } else {
-    mainWindow.loadFile('dist/index.html');
+    mainWindow.loadFile("dist/index.html");
   }
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  app.on('activate', function () {
+  app.on("activate", function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+
+  app.on("web-contents-created", (event, contents) => {
+    // Disable navigation
+    contents.on("will-navigate", (event) => {
+      event.preventDefault();
+    });
+
+    // Disable new window creation
+    contents.setWindowOpenHandler(() => {
+      return { action: "deny" };
+    });
   });
 });
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
 });
--- packages/renderer/src/index.tsx
+++ packages/renderer/src/index.tsx
@@ -1,7 +1,7 @@
 // src/renderer/index.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 
 const container = document.getElementById("root");
 if (!container) {
